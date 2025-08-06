6788fef7dc5b1fc9fc85e4e9f87db1a1b6821c1c
Couvar <krause96@t-online.de>
2025-08-06 09:05:23 +0200

    Create newuser.sh

---
+++ secure-debian/newuser.sh
@@ -0,0 +1,11 @@
+USERNAME="localadmin"
+SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGGzW3voGAIx1EzeT4juFRns0hLuADU52MAiV6Gx210t atd\r.krause@W-BS-RKR"
+
+adduser --disabled-password --gecos "" $USERNAME
+usermod -aG sudo $USERNAME
+
+mkdir -p /home/$USERNAME/.ssh
+echo "$SSH_KEY" > /home/$USERNAME/.ssh/authorized_keys
+chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+chmod 700 /home/$USERNAME/.ssh
+chmod 600 /home/$USERNAME/.ssh/authorized_keys
