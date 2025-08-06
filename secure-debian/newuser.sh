7bfd5499ef8b6b5dc929a69aaa8bff77deab28b7
Couvar <krause96@t-online.de>
2025-08-06 09:08:50 +0200

    Update newuser.sh

--- secure-debian/newuser.sh
+++ secure-debian/newuser.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Skript zum Anlegen eines neuen Benutzers mit SSH-Key
+
 USERNAME="localadmin"
 SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGGzW3voGAIx1EzeT4juFRns0hLuADU52MAiV6Gx210t atd\r.krause@W-BS-RKR"
 
@@ -9,3 +12,5 @@ echo "$SSH_KEY" > /home/$USERNAME/.ssh/authorized_keys
 chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
 chmod 700 /home/$USERNAME/.ssh
 chmod 600 /home/$USERNAME/.ssh/authorized_keys
+
+echo "User $USERNAME wurde erstellt und SSH-Key hinzugefügt."
