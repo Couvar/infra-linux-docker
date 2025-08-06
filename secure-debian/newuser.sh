06e92dbb2d4f5a4999b1182c119b0e9cb629f9f0
Couvar <krause96@t-online.de>
2025-08-06 09:24:37 +0200

    Update newuser.sh

--- secure-debian/newuser.sh
+++ secure-debian/newuser.sh
@@ -4,6 +4,19 @@
 USERNAME="localadmin"
 SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGGzW3voGAIx1EzeT4juFRns0hLuADU52MAiV6Gx210t atd\r.krause@W-BS-RKR"
 
+# Prüfe, ob nötige Pakete installiert sind
+echo "[*] Überprüfe benötigte Pakete..."
+
+REQUIRED_PACKAGES=("adduser" "login")
+
+for pkg in "${REQUIRED_PACKAGES[@]}"; do
+    if ! command -v "${pkg}" >/dev/null 2>&1; then
+        echo "[+] Installiere fehlendes Paket: $pkg"
+        apt update
+        apt install -y "$pkg"
+    fi
+done
+
 adduser --disabled-password --gecos "" $USERNAME
 usermod -aG sudo $USERNAME
 
